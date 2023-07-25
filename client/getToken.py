5550cb59d3d496981550a603d12d98f4ee2bd39e
damianbento <damianbento@gmail.com>
2023-07-25 14:52:22 -0300

    adding cliente py

---
+++ client/getToken.py
@@ -0,0 +1,36 @@
+from getpass import getpass
+import requests
+
+def realizar_solicitud(url, data):
+    headers = {
+        "Content-Type": "application/json"
+    }
+    response = requests.post(url, json=data, headers=headers)
+    return response
+
+def main():
+    try:
+        username = input("Ingresa el nombre de usuario: ")
+        password = getpass("Ingrese el password: ")
+        url = "http://200.45.133.21:3000/api/login"  # Reemplaza esto con la URL de tu API
+        data = {
+            username: "usuario",
+            password: "contrasena"
+
+        }
+
+        response = realizar_solicitud(url, data)
+
+        if response.status_code == 200:
+            print("Solicitud exitosa:")
+            print(response.json())
+        else:
+            print("Error en la solicitud:")
+            print(f"Código de estado: {response.status_code}")
+            print(response.json())
+
+    except Exception as e:
+        print("Error:", e)
+
+if __name__ == "__main__":
+    main()
